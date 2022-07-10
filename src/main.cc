d0d34332bbd77277806cf567d9bb645eac366161
Aleksey Timin <atimin@gmail.com>
2022-07-10 16:53:47 +0200

    fix cpplint

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,15 @@
 // Copyright 2022 Alexey Timin
 #include <CLI/CLI.hpp>
+
 #include <cstring>
 
 #include "reduct_cli/app.h"
 
 int main(int argc, char* argv[]) {
   auto app = reduct::cli::IApp::Build();
-  std::vector<std::string> args(argc-1);
+  std::vector<std::string> args(argc - 1);
   for (int i = 1; i < argc; ++i) {
-    args[i-1] = std::string(argv[i], std::strlen(argv[i]));
+    args[i - 1] = std::string(argv[i], std::strlen(argv[i]));
   }
 
   auto ret = app->Parse(std::move(args));
